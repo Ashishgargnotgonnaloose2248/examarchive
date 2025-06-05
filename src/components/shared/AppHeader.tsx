70b157e8cc64aaccbbd494136a0a495e8e5a72c3
Ashish Garg <ashishgargofficial6548@gmail.com>
2025-06-05 13:20:08 +0000

    make it 40x40

--- src/components/shared/AppHeader.tsx
+++ src/components/shared/AppHeader.tsx
@@ -12,11 +12,11 @@ import { ThemeToggle } from './ThemeToggle';
 
 const CollegeLogo = () => (
   <Image
-    src="/MITS LOGO PNG.png" // Updated to the correct filename
+    src="/MITS LOGO PNG.png"
     alt="Exam Archive College Logo"
-    width={32} // Set the desired width
-    height={32} // Set the desired height
-    className="h-8 w-8" // Tailwind classes for sizing
+    width={40} // Set the desired width
+    height={40} // Set the desired height
+    className="h-10 w-10" // Tailwind classes for sizing (h-10 w-10 = 2.5rem = 40px)
     unoptimized={true} // Added in case there are issues with optimization and spaces
   />
 );
